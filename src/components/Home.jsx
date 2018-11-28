c1dc95cf0ffcfaf7df869e33958b0f1b4717d252
Your Name <you@example.com>
2018-11-28 09:45:37 -0800

    second commit Full stack app

---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = (props) => (
+    <div className="Home">
+        <h1>Welcome to MemoCards App!</h1>
+        <p>Please sign up/log in to create your personal study set.</p>
+    </div>
+)
+
+export default Home;
